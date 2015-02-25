4c1a7138f119130178fc4db025a658045bb74e8d
Yi <yi.irene.li@gmail.com>
2015-02-25 00:05:30 -0500

    Initial Commit

---
+++ EyeXam/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EyeXam
+//
+//  Created by Yi on 2015-02-25.
+//  Copyright (c) 2015 University of Toronro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ EyeXam/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EyeXam
+//
+//  Created by Yi on 2015-02-25.
+//  Copyright (c) 2015 University of Toronro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
