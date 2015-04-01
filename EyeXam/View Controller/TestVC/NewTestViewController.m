1b33619dfa28bf640f01949770ffb113a11561ac
linxy128 <linxy128@gmail.com>
2015-03-31 22:02:48 -0400

    added sound effect

--- EyeXam/View Controller/TestVC/NewTestViewController.m
+++ EyeXam/View Controller/TestVC/NewTestViewController.m
@@ -128,33 +128,33 @@
     AudioServicesCreateSystemSoundID ((__bridge CFURLRef)wrong_soundURL,&_wrong_soundID);
     
     //start calibration phrase
-    NSString *calibration_soundPath = [[NSBundle mainBundle]pathForResource:@"SoundResource/calibration" ofType:@"wav"];
+    NSString *calibration_soundPath = [[NSBundle mainBundle]pathForResource:@"calibration" ofType:@"wav"];
     NSURL *calibration_soundURL = [NSURL fileURLWithPath:calibration_soundPath];
     AudioServicesCreateSystemSoundID ((__bridge CFURLRef)calibration_soundURL,&_calibration_soundID);
     
     //up
-    NSString *up_soundPath = [[NSBundle mainBundle]pathForResource:@"SoundResource/up" ofType:@"wav"];
+    NSString *up_soundPath = [[NSBundle mainBundle]pathForResource:@"up" ofType:@"wav"];
     NSURL *up_soundURL = [NSURL fileURLWithPath:up_soundPath];
     AudioServicesCreateSystemSoundID ((__bridge CFURLRef)up_soundURL,&_up_soundID);
-    //down
-    NSString *down_soundPath = [[NSBundle mainBundle]pathForResource:@"SoundResource/down" ofType:@"wav"];
+//    //down
+    NSString *down_soundPath = [[NSBundle mainBundle]pathForResource:@"down" ofType:@"wav"];
     NSURL *down_soundURL = [NSURL fileURLWithPath:down_soundPath];
     AudioServicesCreateSystemSoundID ((__bridge CFURLRef)down_soundURL,&_down_soundID);
-    //left
-    NSString *left_soundPath = [[NSBundle mainBundle]pathForResource:@"SoundResource/left" ofType:@"wav"];
+//    //left
+    NSString *left_soundPath = [[NSBundle mainBundle]pathForResource:@"left" ofType:@"wav"];
     NSURL *left_soundURL = [NSURL fileURLWithPath:left_soundPath];
     AudioServicesCreateSystemSoundID ((__bridge CFURLRef)left_soundURL,&_left_soundID);
-    //right
-    NSString *right_soundPath = [[NSBundle mainBundle]pathForResource:@"SoundResource/right" ofType:@"wav"];
+//    //right
+    NSString *right_soundPath = [[NSBundle mainBundle]pathForResource:@"right" ofType:@"wav"];
     NSURL *right_soundURL = [NSURL fileURLWithPath:right_soundPath];
     AudioServicesCreateSystemSoundID ((__bridge CFURLRef)right_soundURL,&_right_soundID);
     
-    //start Test
-    NSString *starttest_soundPath = [[NSBundle mainBundle]pathForResource:@"SoundResource/starttest" ofType:@"wav"];
+//    //start Test
+    NSString *starttest_soundPath = [[NSBundle mainBundle]pathForResource:@"starttest" ofType:@"wav"];
     NSURL *starttest_soundURL = [NSURL fileURLWithPath:starttest_soundPath];
     AudioServicesCreateSystemSoundID ((__bridge CFURLRef)starttest_soundURL,&_starttest_soundID);
-    //change Eye
-    NSString *changeeye_soundPath = [[NSBundle mainBundle]pathForResource:@"SoundResource/changeeye" ofType:@"wav"];
+//    //change Eye
+    NSString *changeeye_soundPath = [[NSBundle mainBundle]pathForResource:@"changeeye" ofType:@"wav"];
     NSURL *changeeye_soundURL = [NSURL fileURLWithPath:changeeye_soundPath];
     AudioServicesCreateSystemSoundID ((__bridge CFURLRef)changeeye_soundURL,&_changeeye_soundID);
     
